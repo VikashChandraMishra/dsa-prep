f586233ac35b02f5938739fdc59b11410fb3ebea
Vikash Chandra Mishra <crimsonsyrus000@gmail.com>
2026-04-07 21:12:42 +0530

    make approach concise

--- Reverse every word in a string/Solution.java
+++ Reverse every word in a string/Solution.java
@@ -1,21 +1,22 @@
-// Reverse every word in a string
 
+import java.util.Arrays;
+
+// Reverse every word in a string
 // Given an input string, containing upper-case and lower-case letters, digits, and spaces( ' ' ). A word is defined as a sequence of non-space characters. The words in s are separated by at least one space.
 // Return a string with the words in reverse order, concatenated by a single space.
 class Solution {
 
     // Very inefficient. Too many objects created. Time Complexity is O(n2). Space Complexity is O(n).
+    // This is more concise and functional than the previous approach.
     public static String reverseString(String s) {
         return new StringBuilder(s).reverse().toString();
     }
 
     public static String reverseWords(String s) {
-        String result = "";
         String[] words = reverseString(s).split("\\s+");
 
-        for (String word : words) {
-            result += reverseString(word) + " ";
-        }
+        Arrays.parallelSetAll(words, i -> reverseString(words[i]));
+        String result = String.join(" ", words);
 
         return result.trim();
     }
