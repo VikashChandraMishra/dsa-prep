345e7bdc61f59251b6ba790b80d02e62bafad59c
Vikash Chandra Mishra <crimsonsyrus000@gmail.com>
2026-04-07 20:40:12 +0530

    add comments

--- Best time to buy and sell stock/Solution.java
+++ Best time to buy and sell stock/Solution.java
@@ -1,6 +1,13 @@
+// Best time to buy and sell stock
+
+// Given an array arr of n integers, where arr[i] represents price of the stock on the ith day. Determine the maximum profit achievable by buying and selling the stock at most once. 
+
+// The stock should be purchased before selling it, and both actions cannot occur on the same day.
+
 
 class Solution {
 
+    // Time complexity is O(n2). Inefficient.
     public static int stockBuySell(int[] arr, int n) {
         int profit = 0;
 
